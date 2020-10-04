0dbf208127d7f2f0be3c80612d88df86fa81d919
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-04 14:48:13 +0800

    fix router exercise 1

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router, Link, NavLink, Route} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom';
 import Home from "./home/Home";
 import MyProfile from "./myProfile/MyProfile";
 import AboutUs from "./aboutUs/AboutUs";
@@ -13,9 +13,9 @@ class App extends Component {
         <Router>
             <header>
                 <ul>
-                    <li><Link to='/about-us'>About Us</Link></li>
-                    <li><Link to='/my-profile'>My Profile</Link></li>
-                    <li><Link to='/'>Home</Link></li>
+                    <li><NavLink to="/about-us" activeClassName="active" >About Us</NavLink></li>
+                    <li><NavLink to="/my-profile" activeClassName="active">My Profile</NavLink></li>
+                    <li><NavLink exact to="/" activeClassName="active"  >Home</NavLink></li>
                 </ul>
             </header>
             <switch>
